c1bbe8b5c3c0053b83d7418844bfcb66e82507f7
cldhfleks2 <cldhfleks2@naver.com>
2024-12-22 00:55:01 +0900

    style[MovieDailyStatRepository]: create jpa method

--- src/main/java/com/cldhfleks2/moviehub/movie/dailystat/MovieDailyStatRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/movie/dailystat/MovieDailyStatRepository.java
@@ -1,7 +1,13 @@
 package com.cldhfleks2.moviehub.movie.dailystat;
 
+import com.cldhfleks2.moviehub.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MovieDailyStatRepository extends JpaRepository<MovieDailyStat, Long> {
-
+    // day로 객체를 조회하는 쿼리
+    @Query("SELECT m FROM MovieDailyStat m WHERE m.day = :day AND m.status = 1")
+    Optional<MovieDailyStat> findByDay(String day);
 }
