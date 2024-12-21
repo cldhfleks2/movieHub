68b281e9b6885b7c9a0f3788d63bbe54de373ed7
cldhfleks2 <cldhfleks2@naver.com>
2024-12-21 14:05:09 +0900

    feat[BasicController/Service]: create getKobiskey logic

--- src/main/java/com/cldhfleks2/moviehub/BasicController.java
+++ src/main/java/com/cldhfleks2/moviehub/BasicController.java
@@ -1,6 +1,7 @@
 package com.cldhfleks2.moviehub;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -17,5 +18,11 @@ public class BasicController {
     @GetMapping("/detail")
     String getDetail() {
         return basicService.getDetail();
+    }
+
+    //JS에서 KOBIS Key를 요청하면 String으로 전달해준다.
+    @GetMapping("/getkobiskey")
+    ResponseEntity<String> getKobiskey() {
+        return basicService.getKobiskey();
     }
 }
--- src/main/java/com/cldhfleks2/moviehub/BasicService.java
+++ src/main/java/com/cldhfleks2/moviehub/BasicService.java
@@ -1,11 +1,15 @@
 package com.cldhfleks2.moviehub;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BasicService {
+    @Value("/kobis.key")
+    private String kobiskey;
 
     String getMain() {
         return "main/main";
@@ -14,4 +18,12 @@ public class BasicService {
     String getDetail() {
         return "detail/detail";
     }
+
+    //JS에서 KOBIS Key를 요청하면 String으로 전달해준다.
+    ResponseEntity<String> getKobiskey() {
+        return ResponseEntity.ok(kobiskey);
+    }
+
+
+
 }
