ffddedd8d9abda8e9e59f11d5e78e7127664f1b4
cldhfleks2 <cldhfleks2@naver.com>
2024-12-23 22:41:44 +0900

    style[BasicService]: remove import

--- src/main/java/com/cldhfleks2/moviehub/BasicController.java
+++ src/main/java/com/cldhfleks2/moviehub/BasicController.java
@@ -1,20 +1,11 @@
 package com.cldhfleks2.moviehub;
 
-import com.cldhfleks2.moviehub.config.SeleniumWebDriverConfig;
-import com.cldhfleks2.moviehub.movie.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @Controller
 @RequiredArgsConstructor
