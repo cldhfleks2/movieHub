207f7aa309314c91b42a788464e87cf51f9fceed
cldhfleks2 <cldhfleks2@naver.com>
2024-12-23 22:16:58 +0900

    style[BasicController]: added comment

--- src/main/java/com/cldhfleks2/moviehub/BasicController.java
+++ src/main/java/com/cldhfleks2/moviehub/BasicController.java
@@ -21,21 +21,25 @@ import java.net.http.HttpResponse;
 public class BasicController {
     private final BasicService basicService;
 
+    //test page GET
     @GetMapping("/test")
     String test(Model model) throws Exception {
         return basicService.test(model);
     }
 
+    //test page GET
 //    @GetMapping("/test2")
 //    String test2(Model model) throws Exception {
 //        return basicService.test2(model);
 //    }
 
+    //메인 페이지 GET
     @GetMapping({"/", "/main"})
     String getMain(Model model) throws Exception {
         return basicService.getMain(model);
     }
 
+    //영화 상세 페이지 GET
     @GetMapping("/detail/{movieCd}")
     String getDetail(@PathVariable String movieCd, Model model, RedirectAttributes redirectAttributes)  throws Exception{
         return basicService.getDetail(movieCd, model, redirectAttributes);
