30efc3d6006de016e9c95ec0d563d961a7259296
cldhfleks2 <cldhfleks2@naver.com>
2024-12-26 22:49:11 +0900

    feat[Member]: create login handler

--- src/main/java/com/cldhfleks2/moviehub/member/MemberController.java
+++ src/main/java/com/cldhfleks2/moviehub/member/MemberController.java
@@ -1,11 +1,28 @@
 package com.cldhfleks2.moviehub.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
+    //회원가입시 아이디 중복검사
+    @PostMapping("/api/checkUsername")
+    ResponseEntity<String> checkUsername(String username) {
+        return  memberService.checkUsername(username);
+    }
+
+    //로그인 페이지 GET
+    @GetMapping("/login")
+    String getLogin(Authentication auth){
+        return memberService.getLogin(auth);
+    }
 
 }
--- src/main/java/com/cldhfleks2/moviehub/member/MemberService.java
+++ src/main/java/com/cldhfleks2/moviehub/member/MemberService.java
@@ -1,10 +1,33 @@
 package com.cldhfleks2.moviehub.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    //회원가입시 아이디 중복검사
+    ResponseEntity<String> checkUsername(String username) {
+        Optional<Member> memberObj = memberRepository.findByUsernameAndStatus(username);
+
+        if(memberObj.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build(); //중복됨을 전달
+        }else{
+            return ResponseEntity.status(HttpStatus.OK).build(); //사용가능을 전달
+        }
+    }
+
+    //로그인 페이지 GET
+    String getLogin(Authentication auth){
+        //인증정보가 없거나 로그인된 상태이면 메인 화면으로 이동
+        if(auth != null && auth.isAuthenticated()) return "redirect:/main";
+        return "member/login";
+    }
 }
