05a7d9349dfb4fb24e2890a83b7950f3972749cf
cldhfleks2 <cldhfleks2@naver.com>
2024-12-26 22:29:30 +0900

    feat[MovieLikeRepo]: create findByMovieIdAndStatus method

--- src/main/java/com/cldhfleks2/moviehub/like/MovieLikeRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/like/MovieLikeRepository.java
@@ -1,7 +1,13 @@
 package com.cldhfleks2.moviehub.like;
 
+import com.cldhfleks2.moviehub.movie.dailystat.MovieDailyStat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieLikeRepository extends JpaRepository<MovieLike, Long> {
-
+    // movie_id로 객체를 조회하는 쿼리
+    @Query("SELECT m FROM MovieLike m WHERE m.movie.id = :movieId AND m.status = 1")
+    List<MovieLike> findByMovieIdAndStatus(Long movieId);
 }
