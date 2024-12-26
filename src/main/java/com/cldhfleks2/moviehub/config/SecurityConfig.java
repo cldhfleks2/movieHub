e6b1edc2a6a1b5a9b5a90f8e211d5d1b256a972a
cldhfleks2 <cldhfleks2@naver.com>
2024-12-26 23:30:02 +0900

    feat[SercurityConfig]: apply spring security using login logic

--- src/main/java/com/cldhfleks2/moviehub/config/SecurityConfig.java
+++ src/main/java/com/cldhfleks2/moviehub/config/SecurityConfig.java
@@ -20,11 +20,11 @@ public class SecurityConfig {
         //csrf 공격 방어기능 OFF
         http.csrf((csrf) -> csrf.disable());
 
-
         //클라이언트 요청에 대해 권한 부여
         http.authorizeHttpRequests((authorize) ->
                 //자원에 대한 요구도 처리 가능
                 authorize.requestMatchers("/**").permitAll() //아직 모든 페이지의 로그인처리 제외
+
 //                authorize.requestMatchers("/css/**", "/js/**", "/images/**", "/uploadImage/**", "/static/**").permitAll()
 //                        .requestMatchers("/", "/main").permitAll()
 //                        .requestMatchers("/register", "/login").permitAll()
@@ -34,13 +34,13 @@ public class SecurityConfig {
 //                        .anyRequest().authenticated()
         );
 
-        // 2. 폼으로 로그인하겠다.
-//        http.formLogin((formLogin)  ->
-//                formLogin.loginPage("/login")
-//                        .usernameParameter("name")
-//                        .passwordParameter("password")
-//                        .defaultSuccessUrl("/main")// 로그인 성공시 갈 url
-//        );
+        //로그인 설정
+        http.formLogin((formLogin)  ->
+                formLogin.loginPage("/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/main")// 로그인 성공시 갈 url
+        );
 
         // 로그아웃은
         http.logout(logout -> logout.logoutUrl("/logout"));
