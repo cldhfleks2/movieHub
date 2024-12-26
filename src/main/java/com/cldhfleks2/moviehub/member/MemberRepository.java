aaab7fe0d2d42a9b9e500560559166a44f352eda
cldhfleks2 <cldhfleks2@naver.com>
2024-12-26 22:50:06 +0900

    feat[MemberRepo]: create findByUsernameAndStatus method

--- src/main/java/com/cldhfleks2/moviehub/member/MemberRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/member/MemberRepository.java
@@ -1,7 +1,13 @@
 package com.cldhfleks2.moviehub.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // username과 일치하는 Member 찾기
+    @Query("SELECT m FROM Member m WHERE m.username = :username AND m.status = 1")
+    Optional<Member> findByUsernameAndStatus(String username);
 
 }
