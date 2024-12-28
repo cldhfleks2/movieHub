6af39300d013e3a5b0c67bee92da24dec2680527
cldhfleks2 <cldhfleks2@naver.com>
2024-12-28 14:12:40 +0900

    feat[MovieReviewLikeRepo]: create findAllByMovieReviewIdAndStatus method

--- src/main/java/com/cldhfleks2/moviehub/review/MovieReviewLikeRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/review/MovieReviewLikeRepository.java
@@ -1,6 +1,12 @@
 package com.cldhfleks2.moviehub.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieReviewLikeRepository extends JpaRepository<MovieReviewLike, Long> {
+    //movieReviewId로 MovieReviewLike 리스트를 조회
+    @Query("SELECT mrl FROM MovieReviewLike mrl WHERE mrl.movieReview.id = :movieReviewId AND mrl.status = 1")
+    List<MovieReviewLike> findAllByMovieReviewIdAndStatus(Long movieReviewId);
 }
