b0859fdb0858724e64b4c7249c5b0a6b02364fe6
cldhfleks2 <cldhfleks2@naver.com>
2024-12-29 14:51:31 +0900

    feat[NotificationRepo]: create findAllByUsernameAndIsReadAndStatus method

--- src/main/java/com/cldhfleks2/moviehub/notification/NotificationRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/notification/NotificationRepository.java
@@ -1,6 +1,14 @@
 package com.cldhfleks2.moviehub.notification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    //username으로 isRead=1이고 Status=1인 Notification리스트를 찾기
+    @Query("SELECT n FROM Notification n WHERE n.receiver.username = :username AND n.isRead = 1 AND n.status = 1")
+    List<Notification> findAllByUsernameAndIsReadAndStatus(String username);
+
 }
