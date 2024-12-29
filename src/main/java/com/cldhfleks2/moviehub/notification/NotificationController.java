c618ab84170b101aa1b8744555bbdd244b81624d
cldhfleks2 <cldhfleks2@naver.com>
2024-12-29 16:44:54 +0900

    feat[notification controller, service]: create notification controller, service

--- src/main/java/com/cldhfleks2/moviehub/notification/NotificationController.java
+++ src/main/java/com/cldhfleks2/moviehub/notification/NotificationController.java
@@ -1,10 +1,27 @@
 package com.cldhfleks2.moviehub.notification;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
+
+    //알림 페이지 GET
+    @GetMapping("/notification")
+    String getNotification(Model model, Authentication auth) {
+        return notificationService.getNotification(model, auth);
+    }
+
+    //알림 모두 읽도록 요청 : 헤더만 새로고침할거임
+    @PostMapping("/api/notification/readAll")
+    String readAllNotification(Model model, Authentication auth) {
+        return notificationService.readAllNotification(model, auth);
+    }
+
 }
--- src/main/java/com/cldhfleks2/moviehub/notification/NotificationService.java
+++ src/main/java/com/cldhfleks2/moviehub/notification/NotificationService.java
@@ -1,10 +1,24 @@
 package com.cldhfleks2.moviehub.notification;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+
+    //알림 페이지 GET
+    String getNotification(Model model, Authentication auth) {
+        return "notification/notification";
+    }
+
+    //알림 모두 읽도록 요청
+    String readAllNotification(Model model, Authentication auth) {
+
+
+        return "header/header";
+    }
 }
