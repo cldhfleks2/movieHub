d015275524b206879b00d0de580c9542d19e239e
cldhfleks2 <cldhfleks2@naver.com>
2024-12-30 11:25:25 +0900

    feat[security config]: change remove "/uploadImage/" path

--- src/main/java/com/cldhfleks2/moviehub/config/SecurityConfig.java
+++ src/main/java/com/cldhfleks2/moviehub/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         //클라이언트 요청에 대해 권한 부여
         http.authorizeHttpRequests((authorize) ->
                 //자원에 대한 요구도 처리 가능
-                authorize.requestMatchers("/css/**", "/js/**", "/image/**", "/uploadImage/**", "/static/**").permitAll()
+                authorize.requestMatchers("/css/**", "/js/**", "/image/**", "/static/**").permitAll()
                         .requestMatchers("/", "/main").permitAll()
                         .requestMatchers("/register", "/login").permitAll()
 //                        .requestMatchers("/detail/**").permitAll()
