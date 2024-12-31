edf0f29994c6ad8aee9dfe5e4882ffbe0639a8f7
cldhfleks2 <cldhfleks2@naver.com>
2024-12-31 13:29:48 +0900

    feat[postRep]: create increaseView method

--- src/main/java/com/cldhfleks2/moviehub/community/PostRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/community/PostRepository.java
@@ -1,7 +1,11 @@
 package com.cldhfleks2.moviehub.community;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @Modifying
+    @Query("UPDATE Post p SET p.view = p.view + 1 WHERE p.id = :postId")
+    void incrementView(Long postId);
 }
