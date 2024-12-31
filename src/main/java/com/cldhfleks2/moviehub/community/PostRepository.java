e17d34d12b3c0b2cd45c9cde0984fac5d08de3d3
cldhfleks2 <cldhfleks2@naver.com>
2024-12-31 15:32:35 +0900

    feat[PostRepository]: findAllAndStatus method

--- src/main/java/com/cldhfleks2/moviehub/community/PostRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/community/PostRepository.java
@@ -1,5 +1,7 @@
 package com.cldhfleks2.moviehub.community;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -8,4 +10,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Modifying
     @Query("UPDATE Post p SET p.view = p.view + 1 WHERE p.id = :postId")
     void incrementView(Long postId);
+
+    @Query("SELECT p FROM Post p WHERE p.status = 1")
+    Page<Post> findAllAndStatus(Pageable pageable);
 }
