31a14cddf65b3fdad137c1edb6e3420184a4ba57
cldhfleks2 <cldhfleks2@naver.com>
2024-12-31 14:24:35 +0900

    feat[MoviehubApplication main fucntion]: apply when app run, setDefaultTimezone to asia/seoul

--- src/main/java/com/cldhfleks2/moviehub/MoviehubApplication.java
+++ src/main/java/com/cldhfleks2/moviehub/MoviehubApplication.java
@@ -1,8 +1,11 @@
 package com.cldhfleks2.moviehub;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling //스케쥴링 사용
@@ -12,4 +15,8 @@ public class MoviehubApplication {
 		SpringApplication.run(MoviehubApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setDefaultTimezone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
