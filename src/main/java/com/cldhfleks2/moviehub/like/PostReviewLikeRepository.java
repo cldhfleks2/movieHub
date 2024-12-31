2365865d05ce8797bd505825a3f6a4af14cc6ec4
cldhfleks2 <cldhfleks2@naver.com>
2025-01-01 00:42:03 +0900

    feat[PostDetail]: showing current user post like info logic

--- src/main/java/com/cldhfleks2/moviehub/like/PostReviewLikeRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/like/PostReviewLikeRepository.java
@@ -7,12 +7,17 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PostReviewLikeRepository extends JpaRepository<PostReviewLike, Long> {
-    //postId로 PostReview List를 가져오되, 생성 날짜로 정렬
+    //reviewId로 PostReview List를 가져옴
     @Query("SELECT prl FROM PostReviewLike prl WHERE prl.review.id = :reviewId AND prl.status = 1")
-    List<PostReviewLike> findAllByReviewId(Long reviewId);
+    List<PostReviewLike> findAllByReviewIdAndStatus(Long reviewId);
 
     //reviewId와 memberId에 해당하는 좋아요 기록을 가져옴. 좋아요 요청되있는것만
     @Query("SELECT prl FROM PostReviewLike prl " +
             "WHERE prl.review.id = :reviewId AND prl.sender.id = :memberId AND prl.status = 1")
+    Optional<PostReviewLike> findByReviewIdAndMemberIdAndStatus(Long reviewId, Long memberId);
+
+    //reviewId와 memberId에 해당하는 좋아요 기록을 가져옴.
+    @Query("SELECT prl FROM PostReviewLike prl " +
+            "WHERE prl.review.id = :reviewId AND prl.sender.id = :memberId")
     Optional<PostReviewLike> findByReviewIdAndMemberId(Long reviewId, Long memberId);
 }
