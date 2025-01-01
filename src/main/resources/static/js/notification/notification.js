a7d1a34ec56b649b8ac3398d20c60d733924c570
cldhfleks2 <cldhfleks2@naver.com>
2025-01-01 13:58:24 +0900

    refactor[notification page]: remove notification before moving it to target location

--- src/main/resources/static/js/notification/notification.js
+++ src/main/resources/static/js/notification/notification.js
@@ -12,10 +12,28 @@ function initialize(){
     $(".headerNotificationWrapper").hide(); //알림 페이지에서는 알림 헤더를 감춤
 }
 
-//알림 클릭시 해당 알림이 생겨난 페이지로 이동
+//알림 클릭시 해당 알림이 생겨난 페이지로 이동 : 알림 먼저 삭제후 이동
 function clickToLocate(){
     $(document).on("click", ".notificationInfo", function (){
-        window.location.href = $(this).data("url");
+        //서버로 알림 삭제 요청
+        const notificationId = $(this).data("notificationid");
+        const nextUrl = $(this).data("url");
+
+        $.ajax({
+            url: "/api/notification/read",
+            method: "post",
+            data: {notificationId: notificationId},
+            success: function (response, textStatus, xhr){
+                if (xhr.status === 200) { //서버에서 알림 삭제가 완료 되면 페이지이동
+                    window.location.href = nextUrl;
+                }
+                console.log("/api/notification/read ajax success")
+            },
+            error: function (xhr){
+                console.log(xhr.responseText);
+                console.log("/api/notification/read ajax failed")
+            }
+        })
     })
 }
 
