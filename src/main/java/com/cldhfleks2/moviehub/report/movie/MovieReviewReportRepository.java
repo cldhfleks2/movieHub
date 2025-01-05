f2950954b69147f9d4553887e23976bd2fc593f4
cldhfleks2 <cldhfleks2@naver.com>
2025-01-05 23:43:20 +0900

    feat[MovieReviewReportRepo]: Create JPQL method for search functionality

--- src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReviewReportRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReviewReportRepository.java
@@ -1,7 +1,35 @@
 package com.cldhfleks2.moviehub.report.movie;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieReviewReportRepository extends JpaRepository<MovieReviewReport, Long> {
+    //신고내역의 reportDetail로 검색
+    @Query("SELECT mrr FROM MovieReviewReport mrr WHERE mrr.reportDetail LIKE %:keyword% AND mrr.status = 1")
+    Page<MovieReviewReport> findByReportDetailContaining(String keyword, Pageable pageable);
 
+    //영화의 movieNm으로 검색
+    @Query("SELECT mrr FROM MovieReviewReport mrr " +
+            "JOIN mrr.movieReview mr " +
+            "JOIN mr.movie movie " +
+            "WHERE movie.movieNm LIKE %:keyword% AND mrr.status = 1")
+    Page<MovieReviewReport> findByMovieNmContaining(String keyword, Pageable pageable);
+
+    //리뷰 작성자의 nickname으로 검색
+    @Query("SELECT mrr FROM MovieReviewReport mrr " +
+            "JOIN mrr.member m " +
+            "WHERE m.nickname LIKE %:keyword% " +
+            "AND mrr.status = 1 " +
+            "AND m.status = 1")
+    Page<MovieReviewReport> findByMemberNicknameContaining(String keyword, Pageable pageable);
+
+    //리뷰 내용으로 검색
+    @Query("SELECT mrr FROM MovieReviewReport mrr " +
+            "JOIN mrr.movieReview mr " +
+            "WHERE mr.content LIKE %:keyword% " +
+            "AND mrr.status = 1" +
+            "AND mr.status = 1")
+    Page<MovieReviewReport> findByMovieReviewContentContaining(String keyword, Pageable pageable);
 }
