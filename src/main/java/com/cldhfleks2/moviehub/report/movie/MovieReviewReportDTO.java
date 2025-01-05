dc0940cd5260054a1a39da9e2b67249d29eeb5bd
cldhfleks2 <cldhfleks2@naver.com>
2025-01-05 23:15:47 +0900

    style[MovieReviewReportDTO]: added field comment

--- src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReviewReportDTO.java
+++ src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReviewReportDTO.java
@@ -10,13 +10,13 @@ import lombok.Setter;
 public class MovieReviewReportDTO {
     private Long movieReviewId;
 
-    private Boolean SPOILER;
-    private Boolean WRONG;
-    private Boolean UNRELATED;
-    private Boolean HARMFUL;
-    private Boolean HATE;
-    private Boolean COPYRIGHT;
-    private Boolean SPAM;
+    private Boolean SPOILER;   // 주요 스포일러 포함
+    private Boolean WRONG;     // 잘못된 정보
+    private Boolean UNRELATED; // 무관한 내용/광고성
+    private Boolean HARMFUL;   // 유해하거나 불건전한 내용
+    private Boolean HATE;      // 혐오 발언
+    private Boolean COPYRIGHT; // 저작권 침해
+    private Boolean SPAM;      // 개인정보 노출
 
     private String reportDetail;
 }
