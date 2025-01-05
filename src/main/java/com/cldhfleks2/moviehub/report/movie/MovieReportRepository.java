8e7de4f24f175d33fc8357e54e40cf187ef1e16f
cldhfleks2 <cldhfleks2@naver.com>
2025-01-05 23:04:20 +0900

    feat[MovieReportRepo]: Create JPQL method for search functionality

--- src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReportRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/report/movie/MovieReportRepository.java
@@ -1,6 +1,19 @@
 package com.cldhfleks2.moviehub.report.movie;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieReportRepository extends JpaRepository<MovieReport, Long> {
+    @Query("SELECT mr FROM MovieReport mr " +
+            "WHERE mr.reportDetail LIKE %:keyword% " +
+            "AND mr.status = 1")
+    Page<MovieReport> findReportDetailByKeyword(String keyword, Pageable pageable);
+
+    @Query("SELECT mr FROM MovieReport mr " +
+            "JOIN mr.movie m " +
+            "WHERE m.movieNm LIKE %:keyword% " +
+            "AND mr.status = 1")
+    Page<MovieReport> findMovieNmByKeyword(String keyword, Pageable pageable);
 }
