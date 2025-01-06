4c6c6d46f3d614ac4ccb1a66d42da37456b09755
cldhfleks2 <cldhfleks2@naver.com>
2025-01-06 15:34:11 +0900

    refactor[detail.js]: remove unused function

--- src/main/resources/static/js/detail/detail.js
+++ src/main/resources/static/js/detail/detail.js
@@ -7,28 +7,6 @@ $(document).ready(function (){
     reportBtn();
 })
 
-//쓸지안쓸지모르는 기능
-function A(){
-    // 날짜 선택 기능
-    $('.date-item').on('click', function () {
-        $('.date-item.active').removeClass('active');
-        $(this).addClass('active');
-        // 여기에 날짜 변경 시 상영시간표 업데이트 로직 추가
-    });
-
-// 지역 선택 시 상세 지역 업데이트
-    $('#region').on('change', function () {
-        const subRegion = $('#subRegion');
-        // 여기에 지역에 따른 상세 지역 업데이트 로직 추가
-    });
-
-// 상영시간 선택 시 이벤트
-    $('.showtime-item').on('click', function () {
-        // 여기에 예매 페이지로 이동하는 로직 추가
-        alert('예매 페이지로 이동합니다.');
-    });
-}
-
 //배우나 감독 이름 클릭시 검색 하러 페이지 이동
 function clickActorSearching(){
     //감독명 검색
