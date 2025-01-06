d25dc162294bfea4ba8a5c017ad12eee71b60879
cldhfleks2 <cldhfleks2@naver.com>
2025-01-06 13:48:26 +0900

    feat[PostReportRepo]: Create JPQL method for search functionality

--- src/main/java/com/cldhfleks2/moviehub/report/post/PostReportRepository.java
+++ src/main/java/com/cldhfleks2/moviehub/report/post/PostReportRepository.java
@@ -1,7 +1,36 @@
 package com.cldhfleks2.moviehub.report.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostReportRepository extends JpaRepository<PostReport, Long> {
+    //신고내역의 reportDetail로 검색
+    @Query("SELECT pr FROM PostReport pr WHERE pr.reportDetail LIKE %:keyword% AND pr.status = 1")
+    Page<PostReport> findByReportDetailContaining(String keyword, Pageable pageable);
 
+    //게시글의 title으로 검색
+    @Query("SELECT pr FROM PostReport pr " +
+            "JOIN pr.post p " +
+            "WHERE p.title LIKE %:keyword% " +
+            "AND pr.status = 1" +
+            "AND p.status = 1")
+    Page<PostReport> findByPostTitleContaining(String keyword, Pageable pageable);
+
+    //게시글의 content로 검색
+    @Query("SELECT pr FROM PostReport pr " +
+            "JOIN pr.post p " +
+            "WHERE p.content LIKE %:keyword% " +
+            "AND pr.status = 1" +
+            "AND p.status = 1")
+    Page<PostReport> findByPostContentContaining(String keyword, Pageable pageable);
+
+    //작성자의 nickname로 검색
+    @Query("SELECT pr FROM PostReport pr " +
+            "JOIN pr.member m " +
+            "WHERE m.nickname LIKE %:keyword% " +
+            "AND pr.status = 1" +
+            "AND m.status = 1")
+    Page<PostReport> findByMemberNicknameContaining(String keyword, Pageable pageable);
 }
