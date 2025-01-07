24a9ea6c32ae1f4b14093c21406ea8a739d812c8
cldhfleks2 <cldhfleks2@naver.com>
2025-01-07 20:08:20 +0900

    fix[ManagerService editPost, ]: Change boolean to Boolean in PostDTO
    [fixed: Fix error in controller due to null values in DTO]

--- src/main/java/com/cldhfleks2/moviehub/community/PostDTO.java
+++ src/main/java/com/cldhfleks2/moviehub/community/PostDTO.java
@@ -18,7 +18,7 @@ public class PostDTO {
     private Member member; //작성자 : postDetail
     private LocalDateTime updateDate; //최근시간 : postDetail
     private Long postId; //postDetail 사용
-    private boolean isAuthor; //글 제작자와 auth가 동일하면 : postDetail
+    private Boolean isAuthor; //글 제작자와 auth가 동일하면 : postDetail
 
     private Long likeCount; //게시글 좋아요 갯수 : community
     private Long reviewCount; //댓글 갯수 : community
